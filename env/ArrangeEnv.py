3357038586c2f4441087bad3da7fdad7c40fc567
Nawaf Alansari <nawafmusic@gmail.com>
2020-07-28 19:28:16 -0400

    Added Sarsa algo to arrange env

--- env/ArrangeEnv.py
+++ env/ArrangeEnv.py
@@ -89,3 +89,112 @@ class ArrangeEnv(gym.Env):
     
     def makeObs(self):
         return np.concatenate([np.array([self.number]), self.array])
+
+
+
+
+#Normalizing the states functions
+def norm_array(array,size):
+    return array/size
+def norm_number(number,size):
+    return number / size
+def norm_time(time,limit):
+    return time / limit
+
+def norm_obs(time,number,array, size,limit):
+    return np.concatenate([
+                          np.array([norm_number(number,size)]),
+                          norm_array(array,size)])
+
+def norm_x(n_obs, action,size):
+    return np.concatenate([np.array([action / size]),n_obs])
+
+
+#q linear functions
+def q_linear(x, w):
+    return np.sum(x * w)
+
+
+#Saarsa algorithm 
+def Sarsa(env, w, alpha=0.01, epsilon=.5,gamma=.99):
+    obs = env.reset()
+    done = False
+    new_w = w
+    
+    
+    #Normalize the initial state 
+    current_n_obs = norm_obs(env.time,env.number, env.array,size,time_limit)
+    #Choose an Initial Action (Again eps-greedy algorithm )
+    coin = np.random.binomial(1,epsilon)
+    if(coin == 1):
+            next_action = env.action_space.sample()
+    else:
+        next_action = 0
+        for action in range(size):
+            if q_linear(norm_x(current_n_obs,action,size),new_w) > q_linear(norm_x(current_n_obs,next_action,size),new_w):
+                next_action = action
+                
+                
+    while not done:  #Lopp for each step in the episode
+        
+        current_action = next_action
+        prev_n_obs = current_n_obs
+        
+        #Take the Action Chosen the previous step, observe next state and reward 
+        obs, reward, done,_ = env.step(current_action)
+        current_n_obs = norm_obs(env.time,env.number, env.array,size,time_limit)
+        
+        
+        
+        
+        #if we are done, last reward and break 
+        
+        if done:
+            x_prev = norm_x(prev_n_obs,current_action,size)
+            new_w = new_w + alpha*(reward - q_linear(x_prev,new_w))*x_prev
+            break
+        
+        
+        #otherwise, choose a new action A', according to Greedy and q_linear
+        coin = np.random.binomial(1,epsilon)
+        if(coin == 1):
+            next_action = env.action_space.sample()
+        else:
+            next_action = 0
+            for action in range(size):
+                if q_linear(norm_x(current_n_obs,action,size),new_w) > q_linear(norm_x(current_n_obs,next_action,size),new_w):
+                    next_action = action
+        
+        #update the weights
+        x_prev = norm_x(prev_n_obs,current_action,size)
+        x_current = norm_x(current_n_obs,next_action,size)
+        new_w = new_w + alpha*(reward + gamma * q_linear(x_current,new_w) - q_linear(x_prev,new_w))*x_prev
+        
+    
+    return new_w
+            
+
+#Extract best action from q function
+
+def best_action(env, w):
+    n_obs = norm_obs(env.time,env.number, env.array,size,time_limit)
+    next_action = 0
+    for action in range(size):
+        if q_linear(norm_x(n_obs,action,size),w) > q_linear(norm_x(n_obs,next_action,size),w):
+            next_action = action
+    return next_action
+
+
+#Example how to use 
+'''
+1)Initialize the w with zeros 
+w = np.zeros(size+1+1)
+
+2)Loop to train the w 
+for _ in range(100):
+    w = Sarsa(env,w)
+
+3)use best_action to find best action at each step
+
+
+'''
